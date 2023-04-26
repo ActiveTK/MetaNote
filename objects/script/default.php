6ad3687d9d33525585ed135bbbf890344db42b4c
ActiveTK <68322433+ActiveTK@users.noreply.github.com>
2023-04-26 20:51:42 +0900

    邪魔かもしれない

--- objects/script/default.php
+++ objects/script/default.php
@@ -93,12 +93,12 @@
           <hr class="featurette-divider">
 
           <div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center" id="about">
-            <h1 class="display-4">本サービスについて</h1>
+            <h1 class="display-4">本サービスの<span class="inpred">三つの特徴</span></h1>
           </div>
 
           <div class="row featurette">
             <div class="col-md-7">
-              <h2 class="featurette-heading"><span class="text-center"><span class="text-muted">数少ない</span>日本向けの論文投稿サイト</span></h2>
+              <h2 class="featurette-heading"><span class="text-center"><span class="text-muted">特徴1.</span> 数少ない<span class="inpblue">日本向けの論文投稿サイト</span></span></h2>
               <p class="lead">本サイトは、「ちょっとしたアイディアを気軽に共有できる」という理念の下に開発されている、数少ない日本語の論文投稿サイトです。
               「査読不要・審査なし」を原則とし、誰でも簡単にアイディアを投稿できます。</p>
             </div>
@@ -108,7 +108,7 @@
 
           <div class="row featurette alignright">
             <div class="col-md-7 order-md-2" align="right">
-              <h2 class="featurette-heading">充実した機能</h2>
+              <h2 class="featurette-heading"><span class="text-muted">特徴2.</span> <span class="inpblue">充実した機能</span></h2>
               <p class="lead">論文の共同編集機能や寄付機能を実装しています。
               また、論文の公開コメントを通じて気軽に執筆者とコンタクトできます。</p>
             </div>
@@ -118,7 +118,7 @@
       
           <div class="row featurette">
             <div class="col-md-7">
-              <h2 class="featurette-heading">著作権の保護</h2>
+              <h2 class="featurette-heading"><span class="text-muted">特徴3.</span> <span class="inpblue">著作権の保護</span></h2>
               <p class="lead">希望する場合には、コイン一枚分の金額でデータのメタデータをブロックチェーンに記録できます。
               ブロックチェーンに書き込まれたデータの改竄はほぼ不可能であり、半永久的に著作の記録が残ります。</p>
             </div>

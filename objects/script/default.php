7546e896692250f2ab4e289aa7457556cef7e0e3
ActiveTK <68322433+ActiveTK@users.noreply.github.com>
2023-04-26 20:45:08 +0900

    integrity

--- objects/script/default.php
+++ objects/script/default.php
@@ -28,7 +28,7 @@
 
     <?=MetaNote_Header_Default()?>
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
     <link rel="stylesheet" href="/css/default.css">
 
     <script src="https://unpkg.com/typewriter-effect@2.18.2/dist/core.js"></script>

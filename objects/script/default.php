27a134af8367eceda422cdbc84e9a14ecd47cba5
ActiveTK <68322433+ActiveTK@users.noreply.github.com>
2023-04-26 20:38:05 +0900

    画像は諦めよう

--- objects/script/default.php
+++ objects/script/default.php
@@ -103,29 +103,15 @@
               <p class="lead">本サイトは、「ちょっとしたアイディアを気軽に共有できる」という理念の下に開発されている、数少ない日本語の論文投稿サイトです。
               「査読不要・審査なし」を原則とし、誰でも簡単にアイディアを投稿できます。</p>
             </div>
-            <div class="col-md-5">
-              <img class="featurette-image img-fluid mx-auto" src="/icon/home.png">
-            </div>
           </div>
       
           <hr class="featurette-divider">
-          <!--
-          <div class="row featurette flex">
-            <p class="lead">本サイトは、「ちょっとしたアイディアを気軽に共有できる」という理念の下に開発されている、数少ない日本語の論文投稿サイトです。
-              「査読不要・審査なし」を原則とし、誰でも簡単にアイディアを投稿できます。</p>
-            <figure class="image"><img src="/icon/home.png"></figure>
-          </div>
-      
-          <hr class="featurette-divider"> -->
-      
+
           <div class="row featurette">
-            <div class="col-md-7 order-md-2">
+            <div class="col-md-7 order-md-2" align="right">
               <h2 class="featurette-heading">充実した機能</h2>
               <p class="lead">論文の共同編集機能や寄付機能を実装しています。
               また、論文の公開コメントを通じて気軽に執筆者とコンタクトできます。</p>
-            </div>
-            <div class="col-md-5 order-md-1">
-              <img class="featurette-image img-fluid mx-auto" src="/icon/home.png">
             </div>
           </div>
      
@@ -136,9 +122,6 @@
               <h2 class="featurette-heading">著作権の保護</h2>
               <p class="lead">希望する場合には、コイン一枚分の金額でデータのメタデータをブロックチェーンに記録できます。
               ブロックチェーンに書き込まれたデータの改竄はほぼ不可能であり、半永久的に著作の記録が残ります。</p>
-            </div>
-            <div class="col-md-5">
-              <img class="featurette-image img-fluid mx-auto" src="/icon/home.png" >
             </div>
           </div>
       

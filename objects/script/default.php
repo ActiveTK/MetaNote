223d369911b7cd69111c4f41099b2ddd0aa38e4d
ActiveTK <68322433+ActiveTK@users.noreply.github.com>
2023-04-26 21:15:21 +0900

    揃えたい

--- objects/script/default.php
+++ objects/script/default.php
@@ -161,6 +161,9 @@
   
         </div>
 
+      </div>
+
+      <div class="container marketing">
         <footer class="pt-4 my-md-5 pt-md-5 border-top">
           <div class="row">
             <div class="col-12 col-md">
@@ -184,7 +187,6 @@
             </div>
           </div>
         </footer>
-
       </div>
 
       <br><br>

42abe505682bbe140457a681868a124281730293
ActiveTK <68322433+ActiveTK@users.noreply.github.com>
2023-04-29 20:10:47 +0900

    pdf編集のUIを変更

--- objects/script/edit.php
+++ objects/script/edit.php
@@ -483,11 +483,42 @@
   }
   else if ( $row["DateType"] === "application/pdf" )
   {
+    $title = htmlspecialchars( $row["ArticleTitle"] );
+    $subTitle = htmlspecialchars( $row["ArticleSubtitle"] );
+    $IndexFromBot = "noindex, follow";
+
     ?>
 <!DOCTYPE html>
-<html>
+<html lang="ja" dir="ltr">
   <head>
-    <title>PDF Config - MetaNote.</title>
+
+    <title><?=$title?> - MetaNote.</title>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <meta name="robots" content="<?=$IndexFromBot?>">
+    <meta name="keywords" content="MetaNote">
+    <meta name="favicon" content="/favicon.ico">
+    <meta name="description" content="<?=$subTitle?>">
+    <meta name="copyright" content="<?=Copyright?>">
+
+    <meta name="twitter:description" content="<?=$subTitle?>">
+    <meta name="twitter:domain" content="<?=Domain?>">
+
+    <meta property="og:description" content="<?=$subTitle?>">
+    <meta property="og:type" content="website">
+    <meta property="og:locale" content="ja_JP">
+
+    <link rel="shortcut icon" href="https://<?=Domain?>/icon/index_16_16.ico" sizes="16x16">
+    <link rel="shortcut icon" href="https://<?=Domain?>/icon/index_32_32.ico" sizes="32x32">
+    <link rel="shortcut icon" href="https://<?=Domain?>/icon/index_64_64.ico" sizes="64x64">
+    <link rel="shortcut icon" href="https://<?=Domain?>/icon/index_192_192.ico" sizes="192x192">
+    <link rel="apple-touch-icon-precomposed" href="https://<?=Domain?>/icon/index_150_150.ico">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
+    <link rel="stylesheet" href="/css/view.css">
+
     <script type="text/javascript" src="/js/ActiveTK.min.js" charset="UTF-8"></script>
     <script type="text/javascript">
 
@@ -509,22 +540,60 @@
         }
 
     </script>
-    <link rel="stylesheet" href="/css/edit.css">
+
   </head>
   <body>
-    <div>
-      <h1>PDF論文の設定</h1>
-      <hr size="1" color="#7fffd4">
-      <div>
-        タイトル: <input type="text" id="title2" class="inputtitle" maxlength="120" placeholder="ここにタイトルを入力してください。。(120文字まで)" value="<?=htmlspecialchars($row["ArticleTitle"])?>" required>
-        <br><br>
-        論文の概要: <br><textarea id="stitle" class="stitle" placeholder="ここに論文の概要入力してください。。(1080文字まで)" required><?=htmlspecialchars($row["ArticleSubtitle"])?></textarea>
-        <br>
-        <input type="button" value="保存" class="saveconf" onclick="saveconf()">
-        <br>
+    <?=Get_Body_Header()?>
+
+    <div class="mainobj" align="center">
+
+      <div class="container marketing">
+        <h1>PDF論文の設定</h1>
+        <hr size="1" color="#7fffd4">
+        <div>
+          タイトル: <input type="text" id="title2" class="inputtitle" maxlength="120" placeholder="ここにタイトルを入力してください。。(120文字まで)" value="<?=htmlspecialchars($row["ArticleTitle"])?>" required>
+          <br><br>
+          論文の概要: <br><textarea id="stitle" class="stitle" placeholder="ここに論文の概要入力してください。。(1080文字まで)" required><?=htmlspecialchars($row["ArticleSubtitle"])?></textarea>
+          <br>
+          <input type="button" value="保存" class="saveconf" onclick="saveconf()">
+          <br>
+        </div>
+        <hr size="1" color="#7fffd4">
       </div>
-      <hr size="1" color="#7fffd4">
+
+      <div class="container marketing">
+        <footer class="pt-4 my-md-5 pt-md-5 border-top">
+          <div class="row">
+            <div class="col-12 col-md">
+              <small class="d-block mb-3 text-muted"><?=Copyright?><br>Developed by ActiveTK.</small>
+            </div>
+            <div class="col-6 col-md">
+              <h5>サイトマップ</h5>
+              <ul class="list-unstyled text-small">
+                <li><a class="text-muted" href="/#about">サイト概要</a></li>
+                <li><a class="text-muted" href="https://github.com/ActiveTK/MetaNote">Githubリポジトリ</a></li>
+                <li><a class="text-muted" href="/#contact">お問い合わせ</a></li>
+              </ul>
+            </div>
+            <div class="col-6 col-md">
+              <h5>その他</h5>
+              <ul class="list-unstyled text-small">
+                <li><a class="text-muted" href="/license">利用規約</a></li>
+                <li><a class="text-muted" href="/privacy">プライバシーポリシー</a></li>
+                <li><a class="text-muted" href="https://profile.activetk.jp/">開発者</a></li>
+              </ul>
+            </div>
+          </div>
+        </footer>
+      </div>
+
+      <br><br>
+
     </div>
+
+    <script src="/js/navbar.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+
   </body>
 </html>
     <?php

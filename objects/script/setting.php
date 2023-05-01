b7eb7433da36ccbd3390b45c182c41ef89ec250e
root <=>
2023-05-01 16:44:20 +0900

    ユーザー設定ページの基本UIを作成

--- objects/script/setting.php
+++ objects/script/setting.php
@@ -4,7 +4,93 @@
    * ユーザー設定ページ
    */
 
-?>
+  $title = "アカウント設定";
+  $subTitle = "アカウントの設定を管理/変更できます。";
 
+  $IndexFromBot = "noindex, nofollow";
+
+?>
+<!DOCTYPE html>
+<html lang="ja" dir="ltr">
+  <head>
+
+    <title><?=$title?> - MetaNote.</title>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <meta name="robots" content="<?=$IndexFromBot?>">
+    <meta name="keywords" content="MetaNote">
+    <meta name="favicon" content="/favicon.ico">
+    <meta name="description" content="<?=$subTitle?>">
+    <meta name="copyright" content="<?=Copyright?>">
+
+    <meta name="twitter:description" content="<?=$subTitle?>">
+    <meta name="twitter:domain" content="<?=Domain?>">
+
+    <meta property="og:description" content="<?=$subTitle?>">
+    <meta property="og:type" content="website">
+    <meta property="og:locale" content="ja_JP">
+
+    <link rel="shortcut icon" href="https://<?=Domain?>/icon/index_16_16.ico" sizes="16x16">
+    <link rel="shortcut icon" href="https://<?=Domain?>/icon/index_32_32.ico" sizes="32x32">
+    <link rel="shortcut icon" href="https://<?=Domain?>/icon/index_64_64.ico" sizes="64x64">
+    <link rel="shortcut icon" href="https://<?=Domain?>/icon/index_192_192.ico" sizes="192x192">
+    <link rel="apple-touch-icon-precomposed" href="https://<?=Domain?>/icon/index_150_150.ico">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
+    <link rel="stylesheet" href="/css/setting.css">
+  </head>
+  <body>
+    <?=Get_Body_Header()?>
+
+    <div class="mainobj" align="center">
+
+      <div class="container marketing">
+        <br><br>
+        <h1><?=$title?></h1>
+        <form action="" method="POST" id="td">
+          <hr size="10" color="#7fffd4">
+          <p>ユーザー名: <input type="text" name="UserName" value="<?=htmlspecialchars( $LocalUser["UserName"] )?>"></p>
+          <input type="submit" value="設定を保存" class="btn2">
+          <hr size="10" color="#7fffd4">
+        </form>
+      </div>
+
+      <div class="container marketing">
+        <footer class="pt-4 my-md-5 pt-md-5 border-top">
+          <div class="row">
+            <div class="col-12 col-md">
+              <small class="d-block mb-3 text-muted"><?=Copyright?><br>Developed by ActiveTK.</small>
+            </div>
+            <div class="col-6 col-md">
+              <h5>サイトマップ</h5>
+              <ul class="list-unstyled text-small">
+                <li><a class="text-muted" href="/#about">サイト概要</a></li>
+                <li><a class="text-muted" href="https://github.com/ActiveTK/MetaNote">Githubリポジトリ</a></li>
+                <li><a class="text-muted" href="/#contact">お問い合わせ</a></li>
+              </ul>
+            </div>
+            <div class="col-6 col-md">
+              <h5>その他</h5>
+              <ul class="list-unstyled text-small">
+                <li><a class="text-muted" href="/license">利用規約</a></li>
+                <li><a class="text-muted" href="/privacy">プライバシーポリシー</a></li>
+                <li><a class="text-muted" href="https://profile.activetk.jp/">開発者</a></li>
+              </ul>
+            </div>
+          </div>
+        </footer>
+      </div>
+
+      <br><br>
+
+    </div>
+
+    <script src="/js/navbar.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+
+  </body>
+</html>
   
 

cb6985feecb042d1ca4464b0cc84494d1bad40ce
ActiveTK <68322433+ActiveTK@users.noreply.github.com>
2023-05-01 21:10:42 +0900

    sitemap.xmlを実装

--- public_html/index.php
+++ public_html/index.php
@@ -73,6 +73,43 @@
       exit();
     }
 
+    if ( _MetaNote_URI_LOW == "sitemap.xml" )
+    {
+      header("Content-Type: application/xml;charset=UTF-8");
+      echo "<" . "?"; ?>xml version="1.0" encoding="UTF-8"?>
+<urlset
+      xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+      xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+      xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
+            http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
+
+<url>
+  <loc>https://metanote.org</loc>
+  <lastmod>2023-05-01T06:31:55+00:00</lastmod>
+  <priority>1</priority>
+</url>
+
+    <?php
+      $Notes = array();
+      try {
+        $Notes = (new PDO(DSN, DB_USER, DB_PASS))->query('select ArticleID, LastUpdateTime from MetaNoteArticles where InPublic = "true" order by LastUpdateTime desc limit 2000;');
+        if ($Notes !== false) { }
+        else die("SQLの実行中にエラーが発生しました。");
+      } catch (\Throwable $e) { die("SQLエラーが発生しました。"); }
+
+      foreach($Notes as $value)
+      {
+    ?>
+<url>
+  <loc>https://metanote.org/article/<?=$value["ArticleID"]?></loc>
+  <lastmod><?=date('c', $value["LastUpdateTime"])?></lastmod>
+  <priority>0.8</priority>
+</url>
+    <?php } ?>
+  </urlset><?php
+      exit();
+    }
+
   }
 
   ini_set( 'session.gc_divisor'    , 1            );

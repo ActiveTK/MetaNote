3ec0f22cd07e7ce197f4ee1b52c7a05d8dd93736
ActiveTK <68322433+ActiveTK@users.noreply.github.com>
2023-05-16 19:09:38 +0900

    pdf.jsを使用

--- objects/script/view.php
+++ objects/script/view.php
@@ -647,7 +647,6 @@
       });
     });
     </script>
-    <script src="/lib/embed-pdf-element/mod.js" type="module"></script>
   </head>
   <body>
     <?=Get_Body_Header()?>
@@ -669,7 +668,43 @@
         <br>
         <hr>
 
-        <embed-pdf src="?pdf" style="width:100%;height:800px;"></embed-pdf>
+        <canvas id="pdf-canvas"></canvas>
+        <script src="/lib/pdf.js/pdf.js"></script>
+        <script>
+
+          pdfjsLib.GlobalWorkerOptions.workerSrc = "/lib/pdf.js/pdf.worker.js";
+
+          const loadingTask = pdfjsLib.getDocument("?pdf");
+          (async () => {
+            const pdf = await loadingTask.promise;
+
+            const page = await pdf.getPage(1);
+            const scale = 1.0;
+            const viewport = page.getViewport({ scale });
+            
+            const outputScale = window.devicePixelRatio || 1;
+
+            const canvas = document.getElementById("pdf-canvas");
+            const context = canvas.getContext("2d");
+
+            canvas.width = Math.floor(viewport.width * outputScale);
+            canvas.height = Math.floor(viewport.height * outputScale);
+            canvas.style.width = Math.floor(viewport.width) + "px";
+            canvas.style.height = Math.floor(viewport.height) + "px";
+
+            const transform = outputScale !== 1 
+              ? [outputScale, 0, 0, outputScale, 0, 0] 
+              : null;
+
+            const renderContext = {
+              canvasContext: context,
+              transform,
+              viewport,
+            };
+            page.render(renderContext);
+          })();
+
+        </script>
 
         <div align="left">
           <hr>
